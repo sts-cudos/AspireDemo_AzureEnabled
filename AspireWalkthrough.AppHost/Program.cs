6e928551c10b25df6d5a1b640de4b864ad84214e
Simeï Steiner <simei.steiner@cudos.ch>
2025-03-17 07:33:51 +0100

    feat: Add Azure Support

--- AspireWalkthrough.AppHost/Program.cs
+++ AspireWalkthrough.AppHost/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var backend = builder.AddProject<Projects.Backend>("backend")
     .WithReplicas(2);
 
+var user = builder.AddParameter("postgresuser", "postgres");
+var pw = builder.AddParameter("postgrespassword", secret: true);
+
 var postgres = builder
-    .AddAzurePostgresFlexibleServer("postgres")
-    .RunAsContainer(cfg => cfg.WithDataVolume("postgresdata", isReadOnly: false));
+    .AddAzurePostgresFlexibleServer("postgres");
+
+if (builder.Environment.IsDevelopment())
+{
+    postgres.RunAsContainer(configure =>
+    {
+        configure
+            .WithDataVolume("postgresdata", isReadOnly: false)
+            .WithContainerName("postgres-local");
+    });
+}
+else{
+    postgres.WithPasswordAuthentication(user, pw);
+}
+
 var aspiredb = postgres.AddDatabase("aspiredb");
 var postgresdb = postgres.AddDatabase(name: "postgresdb", databaseName: "postgres");
 
@@ -23,10 +41,17 @@ var webapi = builder.AddProject<Projects.WebApi>("webapi")
     .WithReference(cache)
     .WaitFor(cache);
 
-var frontend = builder.AddNpmApp("frontend", "../Frontend")
+var frontend = builder.AddNpmApp("frontend", "../AngularFE")
     .WithReference(webapi)
-    .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
+
+if(builder.Environment.IsDevelopment())
+{
+    frontend.WithHttpEndpoint(env: "PORT");
+}
+else{
+    frontend.WithHttpsEndpoint(env: "PORT", port: 80);
+}
 
 builder.Build().Run();
